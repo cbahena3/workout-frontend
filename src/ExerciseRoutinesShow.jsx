2ffe29a71aa3ab947636684bd29ebfbc0d7303bf
Cristian B <cris.bahena77@gmail.com>
2024-02-12 11:22:39 -0600

    added css to exercise routine show

--- src/ExerciseRoutinesShow.jsx
+++ src/ExerciseRoutinesShow.jsx
@@ -12,28 +12,35 @@ export function ExerciseRoutinesShow(props) {
   };
 
   return(
-    <div>
+    <div className="modal-body">
       <h1>Exercise Routine information</h1>
       <h2>Exercise ID: {props.currentExcerciseRoutine.exercise_id}</h2>
       <p> Routine ID: {props.currentExcerciseRoutine.routine_id} </p>
       <p>Sets: {props.currentExcerciseRoutine.sets}</p>
       <p>Reps: {props.currentExcerciseRoutine.reps}</p>
-      <form onSubmit={handleSubmit}>
+      <form className="row g-3" onSubmit={handleSubmit}>
         <div>
-          Exercise ID: <input defaultValue={props.currentExcerciseRoutine.exercise_id} name="exercise_id" type="text" />
+          <label htmlFor="exercise_id" className="form-label">Exercise Id:</label>
+          <input defaultValue={props.currentExcerciseRoutine.exercise_id} name="exercise_id" type="text" className="form-control" id="exercise_id"/>
         </div>
         <div>
-          Routine ID: <input defaultValue={props.currentExcerciseRoutine.routine_id} name="routine_id" type="text" />
+          <label htmlFor="routine_id" className="form-label">Routine Id:</label>
+          <input defaultValue={props.currentExcerciseRoutine.routine_id} name="routine_id" type="text" className="form-control" id="routine_id"/>
         </div>
         <div>
-          Sets: <input defaultValue={props.currentExcerciseRoutine.sets} name="sets" type="text" />
+
+          <label htmlFor="sets" className="form-label">Sets:</label>
+          <input defaultValue={props.currentExcerciseRoutine.sets} name="sets" type="text" className="form-control" id="sets"/>
         </div>
         <div>
-          Reps: <input defaultValue={props.currentExcerciseRoutine.reps} name="reps" type="text" />
+
+          <label htmlFor="reps" className="form-label">Reps:</label>
+          <input defaultValue={props.currentExcerciseRoutine.reps} name="reps" type="text" className="form-control" id="reps"/>
         </div>
-        <button type="submit">Update Exercise For Routine</button>
+        <button type="submit" className="d-grid gap-2 col-6 mx-auto btn btn btn-primary" >Save Changes</button>
       </form>
-        <button onClick={handleClick}>Remove exercise from routine</button>
+      <br />
+        <button className="d-grid gap-2 col-6 mx-auto btn btn-secondary" onClick={handleClick}>Remove Exercise From Routine</button>
     </div>
   );
 }
