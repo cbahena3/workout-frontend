e0d65284ec8c888ba5eb19e862a50b4a00d7681d
Cristian B <cris.bahena77@gmail.com>
2024-02-12 10:25:48 -0600

    added css to users index

--- src/UsersIndex.jsx
+++ src/UsersIndex.jsx
@@ -3,14 +3,16 @@ export function UsersIndex(props) {
 
   return (
     <div>
-      <h1>All users</h1>
+      <h1 className="text-center">All users</h1>
       {props.users.map((user) => (
-        <div key={user.id}>
-          <h2>{user.name}</h2>
-          <img src={user.image_url} />
-          <p>name: {user.name}</p>
-          <p>email: {user.email}</p>
-          <button onClick={() => props.onShowUser(user)}>More info</button>
+        <div key={user.id} className="card container mb-3" style={{width: "25rem"}}>
+          <h2 className="card-title">{user.name}</h2>
+          <div className="card-body text-center">
+            <img className="card-img-top" src={user.image_url} />
+            <p className="card-text">email: {user.email}</p>
+            <button onClick={() => props.onShowUser(user)} className="btn btn-primary shadow">More info</button>
+          </div>
+            
         </div>
       ))}
     </div>
