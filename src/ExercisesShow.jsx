c417a90b2f22bfcd2d514cc3b818ba90cbd3508a
Cristian B <cris.bahena77@gmail.com>
2024-02-12 10:40:06 -0600

    added css to users show

--- src/ExercisesShow.jsx
+++ src/ExercisesShow.jsx
@@ -50,21 +50,3 @@ export function ExercisesShow(props) {
     </div>
   );
 }
-
-
-{/* <form onSubmit={handleSubmit}>
-        <div>
-          Name: <input defaultValue={props.exercise.name} name="name" type="text" />
-        </div>
-        <div>
-          Image: <input defaultValue={props.exercise.image_url} name="image_url" type="text" />
-        </div>
-        <div>
-          Video: <input defaultValue={props.exercise.video_url} name="video_url" type="text" />
-        </div>
-        <div>
-          Description: <input defaultValue={props.exercise.description} name="description" type="text" />
-        </div>
-        <button type="submit">Save Changes</button>
-      </form>
-        <button onClick={handleClick}>Delete exercise</button> */}
--- src/UsersShow.jsx
+++ src/UsersShow.jsx
@@ -11,32 +11,40 @@ export function UsersShow(props) {
 
   return (
     
-    <div>
+    <div className="modal-body">
       <h1>{props.user.name}'s information</h1>
       <p><img src = {props.user.image_url}/></p>
       <p>Name: {props.user.name}</p>
       <p>email: {props.user.email}</p>
       <div>
         <h3>Update Info:</h3>
-        <form onSubmit={handleSubmit}>
-          <div>
-            Name: <input defaultValue={props.user.name} name="name" type="text" />
+        <form onSubmit={handleSubmit} className="row g-3">
+          <div className="col-6">
+            <label htmlFor="name" className="form-label">Name:</label>
+            <input defaultValue={props.user.name} name="name" type="text" className="form-control" id="name"/>
+          </div>
+          <div className="col-6">
+              <label htmlFor="image_url" className="form-label">Profile Photo:</label>
+              <input defaultValue={props.user.image_url} name="image_url" type="text" className="form-control" id="image_url"/>
+          </div>
+          <div className="col-12">
+
+            <label htmlFor="email" className="form-label">Email:</label>
+            <input defaultValue={props.user.email} name="email" type="text" className="form-control" id="email" />
           </div>
           <div>
-            Profile Photo: <input defaultValue={props.user.image_url} name="image_url" type="text" />
+
+            <label htmlFor="password" className="form-label">New Password:</label>
+            <input defaultValue={props.user.password} name="password" type="password" className="form-control" id="password"  maxLength={20}/>
           </div>
           <div>
-            Email: <input defaultValue={props.user.email} name="email" type="text" />
+            <label htmlFor="password_confirmation" className="form-label">Password Confirmation:</label>
+            <input defaultValue={props.user.password_confirmation} name="password_confirmation" type="password" className="form-control" id="password_confirmation"  maxLength={20}/>
           </div>
-          <div>
-            Password: <input defaultValue={props.user.password} name="password" type="password" />
-          </div>
-          <div>
-            Password Confirmation: <input defaultValue={props.user.password_confirmation} name="password_confirmation" type="password" />
-          </div>
-          <button type="submit">Update user</button>
+          <button type="submit" className="d-grid gap-2 col-6 mx-auto btn btn btn-primary" >Save Changes </button>
         </form>
-          <button onClick={handleClick}>Delete user</button>
+          <br />
+          <button onClick={handleClick} className="d-grid gap-2 col-6 mx-auto btn btn-secondary">Delete user</button>
       </div>
     </div>
   );
