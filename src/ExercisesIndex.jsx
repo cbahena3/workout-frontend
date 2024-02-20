98d90ba6764a083af08863906ad4f97f9cbaf419
Cristian B <cris.bahena77@gmail.com>
2024-02-20 12:17:12 -0600

    added used muscles to all exercises to show

--- src/ExercisesIndex.jsx
+++ src/ExercisesIndex.jsx
@@ -1,22 +1,32 @@
 /* eslint-disable react/prop-types */
-// import axios from "axios";
-// import { useEffect, useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export function ExercisesIndex(props){
-  // const [muscleGroups, setMuscleGroups] = useState([]);
-  // const getMuscleGroup = () => {
-  //   axios.get("http://localhost:3000/muscle_groups.json").then((response) => {
-  //   console.log(response.data);
-  //   setMuscleGroups(response.data);
-  //   })
-  // }
-  // useEffect(getMuscleGroup, []);
+  const [muscleGroups, setMuscleGroups] = useState([]);
+  const getMuscleGroup = () => {
+    axios.get("/muscle_groups.json").then((response) => {
+    setMuscleGroups(response.data);
+    })
+  }
+  useEffect(getMuscleGroup, []);
+
+  const [usedMuscles, setUsedMuscles] = useState([]);
+  const getUsedMuscles = () => {
+    axios.get("/used_muscles.json").then((response) => {
+    setUsedMuscles(response.data);
+    })
+  }
+  useEffect(getUsedMuscles, []);
+
+  const getMuscleGroupName = (groupId) => {
+    const muscleGroup = muscleGroups.find(group => group.id === groupId);
+    return muscleGroup ? muscleGroup.name : "Unknown Muscle Group";
+  }
 
   return(
     <div>
       <h1 className="text-center" >All Exercises</h1>
-      
-      
       {props.exercises.map((exercise) => (
       <div className="card container mb-4 border border-primary-subtle" style={{width: "25rem"}} key={exercise.id}>       
         <img src={exercise.image_url} className="card-img-top"/>
@@ -24,16 +34,16 @@ export function ExercisesIndex(props){
           <h5 className="card-title">{exercise.name}</h5>
           <p className="card-text">{exercise.description}</p>
           <p className="card-text">Muscles Used:   
-                
-          {/* {props.exercise.used_muscle.map((used_muscle)=>(
-            <div key={used_muscle.id} >
-              <ol>
-                <li>{used_muscle.muscle_group.name}</li>
-              </ol>
-            </div>
-          ))} */}
-
-
+          {usedMuscles.map((used_muscle) => {
+            if (exercise.id === used_muscle.exercise_id) {
+              return (
+                <div key={used_muscle.id}>
+                  <p>{getMuscleGroupName(used_muscle.muscle_group_id)}</p>
+                </div>
+              );
+            }
+            return null;
+          })}
           </p>
           <button onClick={() => props.onShowExercise(exercise)} className="btn btn-primary shadow">More info</button>        
         </div>
@@ -42,3 +52,7 @@ export function ExercisesIndex(props){
     </div>
   )
 }
+
+
+
+
