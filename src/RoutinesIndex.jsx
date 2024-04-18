7056f06f69bf53eebdc66ccd07f0429a3ca0caa7
Cristian B <cris.bahena77@gmail.com>
2024-04-18 00:07:46 -0500

    added css to routines index and routines new

--- src/RoutinesIndex.jsx
+++ src/RoutinesIndex.jsx
@@ -4,20 +4,20 @@ export function RoutinesIndex(props){
     <div>
       <h1 className="text-center" >Routines</h1>
       {props.routines.map((routine)=>(
-        <div className="card container mb-3 border border-primary-subtle" style={{width: "40rem"}} key={routine.id}>
+        <div key = {routine.id} className="card container mb-3 border border-primary-subtle" style={{width: "40rem", paddingBottom: '20px'}}>
           <div className="card-body text-center">
             <h2 className="card-title">{routine.name} </h2>
             <hr />
             
           </div>
-            {routine.exercise_routines.map((exercise_routine)=>
-            <div className="card-body text-center"key={exercise_routine.id}>
-              <p className="card-text">Exercise: {exercise_routine.name}</p>
-              <p className="card-text">Sets: {exercise_routine.sets}</p>
-              <p className="card-text">Reps: {exercise_routine.reps}</p>
-              <hr />
-            </div>
-            )} 
+          {routine.exercise_routines.map((exercise_routine)=>
+          <div className="card-body text-center" key={exercise_routine.id}>
+            <p className="card-text">Exercise: {exercise_routine.name}</p>
+            <p className="card-text">Sets: {exercise_routine.sets}</p>
+            <p className="card-text">Reps: {exercise_routine.reps}</p>
+            <hr />
+          </div>
+          )} 
           <button onClick={() => props.onShowRoutine(routine)} className="btn btn-primary shadow"> More Info </button>
         </div>
       ))}
--- src/RoutinesNew.jsx
+++ src/RoutinesNew.jsx
@@ -8,15 +8,17 @@ export function RoutinesNew(props){
 
   return(
     <div>
-      <form className="row g-3" onSubmit={handleSubmit}>
-        <h2>Create New Routine:</h2>
-
+      <h2 className="text-center" style={{paddingTop:'30px'}}>Create New Routine:</h2>
+      <form className="row g-3 justify-content-center" onSubmit={handleSubmit} style={{padding:'30px'}}>
         <div className="col-md-6">
           <label htmlFor="name" className="form-label">Routine Name:</label>
           <input name="name" type="text" className="form-control" id="name" placeholder="Pull Day"/>
         </div>
-        <button type="submit" className="btn btn-primary">Create routine</button>
+        <div className="col-md-6 d-flex align-items-end">
+          <button type="submit" className="btn btn-primary" style={{width: '100%', height: '4vh'}}>Create routine</button>
+        </div>
       </form>
+
     </div>
   )
 }
